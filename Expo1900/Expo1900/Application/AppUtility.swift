1ff3540eb41c4611e35d6a7215b8ac1daae820be
wongbingg <wb277@naver.com>
2022-11-07 14:55:28 +0900

    style : "AppUtility", "AppDelegate" 컨벤션 수정
    - 메서드 파라미터 줄바꿈
    - 개행 수정

--- Expo1900/Expo1900/Application/AppUtility.swift
+++ Expo1900/Expo1900/Application/AppUtility.swift
@@ -11,7 +11,6 @@ import UIKit
 struct AppUtility {
     
     static func lockOrientation(_ orientation: UIInterfaceOrientationMask) {
-        
         if let delegate = UIApplication.shared.delegate as? AppDelegate {
             delegate.orientationLock = orientation
         }
